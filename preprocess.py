58b89534a8fca91d01614062e30b7b0303bf2afb
SvGeloven <sander.vangeloven@opentaal.org>
2015-04-30 17:16:15 +0200

    Improved preoprocessing.

--- preprocess.py
+++ preprocess.py
@@ -12,5 +12,11 @@ if __name__ == '__main__':
     sort_order_file = open('sort-order.txt', 'w')
     with open('sort-order.tsv', 'r') as sort_order_annotated_file:
         for line in sort_order_annotated_file:
-            if line[0] != '#':    
-                sort_order_file.write('{}\n'.format(line.split('	')[0]))
+            if line != '\n' and line[0] != '#':
+                sort_order_file.write('{}\n'.format(line[:-1].split('	')[0]))
+
+    reference_sorted_file = open('reference-sorted.txt', 'w')
+    with open('reference-sorted.tsv', 'r') as reference_sorted_annotated_file:
+        for line in reference_sorted_annotated_file:
+            if line != '\n' and line[0] != '#':
+                reference_sorted_file.write('{}\n'.format(line[:-1].split('	')[0]))
