69dc7b394ca67311400f8a7a1b0c968bf7ea7130
SvGeloven <sander.vangeloven@opentaal.org>
2015-05-02 19:02:42 +0200

    Approach retrograde sorting and scientific chars

--- compare.sh
+++ compare.sh
@@ -19,3 +19,15 @@ diff -Nup reference-sorted.txt coreutils-sort-version.txt > coreutils-sort-versi
 # (order of these options is not important)
 cat reference-sorted.txt|sort -dV > coreutils-sort-dict-version.txt
 diff -Nup reference-sorted.txt coreutils-sort-dict-version.txt > coreutils-sort-dict-version.diff
+
+# custom sort
+cat reference-sorted.txt|sed -f away.sed|sort|sed -f back.sed > coreutils-sort-custom.txt
+diff -Nup reference-sorted.txt coreutils-sort-custom.txt > coreutils-sort-custom.diff
+
+# custom sort 2
+cat reference-sorted.txt|sed -f away2.sed|sort|sed -f back2.sed > coreutils-sort-custom2.txt
+diff -Nup reference-sorted.txt coreutils-sort-custom2.txt > coreutils-sort-custom2.diff
+
+# retrograde sort
+rev reference-sorted-retrograde.txt|sort|rev > coreutils-sort-retrograde.txt
+diff -Nup reference-sorted-retrograde.txt coreutils-sort-retrograde.txt > coreutils-sort-retrograde.diff
