5fac2c40afec4396b6b0e2bf0b4eb9bf5121157d
SvGeloven <sander.vangeloven@opentaal.org>
2015-05-26 17:54:32 +0200

    Moved some parts to other projects

--- compare.sh
+++ compare.sh
@@ -1,4 +1,4 @@
-cd reference
+cd references
 ./preprocess.py
 cd ..
 
@@ -9,63 +9,63 @@ export LC_COLLATE="nl_NL.UTF-8"
 ### Pyhton ###
 
 # sort default
-./python-sort-default.py reference/sort.txt > result/python-sort-default.txt
-diff -Nup reference/sort.txt result/python-sort-default.txt > result/python-sort-default.diff
+./python-sort-default.py references/sort.txt > results/python-sort-default.txt
+diff -Nup references/sort.txt results/python-sort-default.txt > differences/python-sort-default.diff
 
 # sort locale
-./python-sort-locale.py reference/sort.txt > result/python-sort-locale.txt
-diff -Nup reference/sort.txt result/python-sort-locale.txt > result/python-sort-locale.diff
+./python-sort-locale.py references/sort.txt > results/python-sort-locale.txt
+diff -Nup references/sort.txt results/python-sort-locale.txt > differences/python-sort-locale.diff
 
 ### coreutils ###
 
 # sort default - according to locale
-cat reference/sort.txt|sort > result/coreutils-sort.txt
-diff -Nup reference/sort.txt result/coreutils-sort.txt > result/coreutils-sort.diff
+cat references/sort.txt|sort > results/coreutils-sort.txt
+diff -Nup references/sort.txt results/coreutils-sort.txt > differences/coreutils-sort.diff
 
 # sort dictionary - ignore leading blanks
-cat reference/sort.txt|sort -d > result/coreutils-sort-dict.txt
-diff -Nup reference/sort.txt result/coreutils-sort-dict.txt > result/coreutils-sort-dict.diff
+cat references/sort.txt|sort -d > results/coreutils-sort-dict.txt
+diff -Nup references/sort.txt results/coreutils-sort-dict.txt > differences/coreutils-sort-dict.diff
 
 # sort version - natural sort of (version) numbers within text
-cat reference/sort.txt|sort -V > result/coreutils-sort-version.txt
-diff -Nup reference/sort.txt result/coreutils-sort-version.txt > result/coreutils-sort-version.diff
+cat references/sort.txt|sort -V > results/coreutils-sort-version.txt
+diff -Nup references/sort.txt results/coreutils-sort-version.txt > differences/coreutils-sort-version.diff
 
 # sort ignore case - fold lower case to upper case characters
-cat reference/sort.txt|sort -f > result/coreutils-sort-fold.txt
-diff -Nup reference/sort.txt result/coreutils-sort-fold.txt > result/coreutils-sort-fold.diff
+cat references/sort.txt|sort -f > results/coreutils-sort-fold.txt
+diff -Nup references/sort.txt results/coreutils-sort-fold.txt > differences/coreutils-sort-fold.diff
 
 # sort dictionary and version sort (order of these options is not important)
-cat reference/sort.txt|sort -dV > result/coreutils-sort-dict-version.txt
-diff -Nup reference/sort.txt result/coreutils-sort-dict-version.txt > result/coreutils-sort-dict-version.diff
+cat references/sort.txt|sort -dV > results/coreutils-sort-dict-version.txt
+diff -Nup references/sort.txt results/coreutils-sort-dict-version.txt > differences/coreutils-sort-dict-version.diff
 
 # reverse sort
-sort -r reference/sort-reverse.txt > result/coreutils-sort-reverse.txt
-diff -Nup reference/sort-reverse.txt result/coreutils-sort-reverse.txt > result/coreutils-sort-reverse.diff
+sort -r references/sort-reverse.txt > results/coreutils-sort-reverse.txt
+diff -Nup references/sort-reverse.txt results/coreutils-sort-reverse.txt > differences/coreutils-sort-reverse.diff
 
 # retrograde sort
-rev reference/sort-retrograde.txt|sort|rev > result/coreutils-sort-retrograde.txt
-diff -Nup reference/sort-retrograde.txt result/coreutils-sort-retrograde.txt > result/coreutils-sort-retrograde.diff
+rev references/sort-retrograde.txt|sort|rev > results/coreutils-sort-retrograde.txt
+diff -Nup references/sort-retrograde.txt results/coreutils-sort-retrograde.txt > differences/coreutils-sort-retrograde.diff
 
 # reverse retrograde sort
-rev reference/sort-reverse-retrograde.txt|sort -r|rev > result/coreutils-sort-reverse-retrograde.txt
-diff -Nup reference/sort-reverse-retrograde.txt result/coreutils-sort-reverse-retrograde.txt > result/coreutils-sort-reverse-retrograde.diff
+rev references/sort-reverse-retrograde.txt|sort -r|rev > results/coreutils-sort-reverse-retrograde.txt
+diff -Nup references/sort-reverse-retrograde.txt results/coreutils-sort-reverse-retrograde.txt > differences/coreutils-sort-reverse-retrograde.diff
 
 
 
 
 
 # sort custom
-cat reference/sort.txt|sed -f substitute.sed|sort|sed -f restore.sed > result/coreutils-sort-custom.txt
-diff -Nup reference/sort.txt result/coreutils-sort-custom3.txt > result/coreutils-sort-custom.diff
+cat references/sort.txt|sed -f substitute.sed|sort|sed -f restore.sed > results/coreutils-sort-custom.txt
+diff -Nup references/sort.txt results/coreutils-sort-custom3.txt > differences/coreutils-sort-custom.diff
 
 # custom sort
-#cat reference/sort.txt|sed -f away.sed|sort|sed -f back.sed > result/coreutils-sort-custom.txt
-#diff -Nup reference/sort.txt coreutils-sort-custom.txt > result/coreutils-sort-custom.diff
+#cat references/sort.txt|sed -f away.sed|sort|sed -f back.sed > results/coreutils-sort-custom.txt
+#diff -Nup references/sort.txt coreutils-sort-custom.txt > differences/coreutils-sort-custom.diff
 
 # custom sort 2
-#cat reference/sort.txt|sed -f away2.sed|sort|sed -f back2.sed > result/coreutils-sort-custom2.txt
-#diff -Nup reference/sort.txt coreutils-sort-custom2.txt > result/coreutils-sort-custom2.diff
+#cat references/sort.txt|sed -f away2.sed|sort|sed -f back2.sed > results/coreutils-sort-custom2.txt
+#diff -Nup references/sort.txt coreutils-sort-custom2.txt > differences/coreutils-sort-custom2.diff
 
 # custom sort 4
-#cat reference/sort.txt|sed -f away4.sed|sort|sed -f back4.sed > result/coreutils-sort-custom4.txt
-#diff -Nup reference/sort.txt coreutils-sort-custom4.txt > result/coreutils-sort-custom4.diff
+#cat references/sort.txt|sed -f away4.sed|sort|sed -f back4.sed > results/coreutils-sort-custom4.txt
+#diff -Nup references/sort.txt coreutils-sort-custom4.txt > differences/coreutils-sort-custom4.diff
